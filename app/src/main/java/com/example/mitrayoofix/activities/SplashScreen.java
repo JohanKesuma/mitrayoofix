61e064efb6fcdfa1b091041535ad7c0b9dbe0b0a
Johan Satria Kesuma <jokesuma@gmail.com>
2020-04-24 14:59:40 +0700

    gabung

--- app/src/main/java/com/example/mitrayoofix/activities/SplashScreen.java
+++ app/src/main/java/com/example/mitrayoofix/activities/SplashScreen.java
@@ -40,7 +40,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, RegisterActivity.class);
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
                 startActivity(intent);
             }
         }, 3000);
